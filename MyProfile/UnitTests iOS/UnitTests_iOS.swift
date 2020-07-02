c1625a3c4b00623f844270231b0c08fa0129e3d2
史 翔新 <x_shi@yumemi.co.jp>
2020-07-03 04:19:26 +0900

    Move ProfileUsecaseTests to shared test directory and restore original UnitTests_iOS

--- MyProfile/UnitTests iOS/UnitTests_iOS.swift
+++ MyProfile/UnitTests iOS/UnitTests_iOS.swift
@@ -6,39 +6,27 @@
 //
 
 import XCTest
-@testable import MyProfile
 
 class UnitTests_iOS: XCTestCase {
-    
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let date20000701 = Date.at(year: 2000, month: 7, day: 1)
-        let date20200630 = Date.at(year: 2020, month: 6, day: 30)
-        let date20200701 = Date.at(year: 2020, month: 7, day: 1)
-        
-        let usecase = ProfileUsecase()
-        XCTAssertNil(usecase.ageOnYear(at: date20000701))
-        
-        usecase.publishedBirthday = date20000701
-        XCTAssertEqual(usecase.ageOnYear(at: date20000701), 0)
-        XCTAssertEqual(usecase.ageOnYear(at: date20200630), 19)
-        XCTAssertEqual(usecase.ageOnYear(at: date20200701), 20)
     }
-    
-}
 
-private extension Date {
-    
-    static func at(year: Int, month: Int, day: Int) -> Date {
-        
-        let components = DateComponents(calendar: .init(identifier: .gregorian),
-                                        year: year,
-                                        month: month,
-                                        day: day)
-        
-        return components.date!
-        
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
     }
-    
+
 }
