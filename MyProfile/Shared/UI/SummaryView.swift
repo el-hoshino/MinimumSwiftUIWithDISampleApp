c44453d11fc887cf65e404669608c61b6d3f03a2
史 翔新 <x_shi@yumemi.co.jp>
2020-07-03 00:17:41 +0900

    Add SummaryView file

---
+++ MyProfile/Shared/UI/SummaryView.swift
@@ -0,0 +1,20 @@
+//
+//  SummaryView.swift
+//  iOS
+//
+//  Created by 史 翔新 on 2020/07/03.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryView()
+    }
+}
