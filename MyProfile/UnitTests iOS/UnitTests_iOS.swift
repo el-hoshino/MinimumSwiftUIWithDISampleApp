a6cff328f9aea60101012d1cd6ae83a5d00c3319
史 翔新 <x_shi@yumemi.co.jp>
2020-07-02 22:39:10 +0900

    Remove unneeded boilerplate code

--- MyProfile/UnitTests iOS/UnitTests_iOS.swift
+++ MyProfile/UnitTests iOS/UnitTests_iOS.swift
@@ -9,15 +9,7 @@ import XCTest
 @testable import MyProfile
 
 class UnitTests_iOS: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -33,12 +25,20 @@ class UnitTests_iOS: XCTestCase {
         XCTAssertEqual(usecase.ageInYear(at: date20200630), 19)
         XCTAssertEqual(usecase.ageInYear(at: date20200701), 20)
     }
+    
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+private extension Date {
+    
+    static func at(year: Int, month: Int, day: Int) -> Date {
+        
+        let components = DateComponents(calendar: .init(identifier: .gregorian),
+                                        year: year,
+                                        month: month,
+                                        day: day)
+        
+        return components.date!
+        
     }
-
+    
 }
