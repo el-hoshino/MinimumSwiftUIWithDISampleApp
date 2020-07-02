da5abf47e2bbef9600b32d230b2db3be4cbd5740
史 翔新 <x_shi@yumemi.co.jp>
2020-07-03 02:28:35 +0900

    Add NameSetupView file

---
+++ MyProfile/Shared/UI/NameSetupView.swift
@@ -0,0 +1,20 @@
+//
+//  NameSetupView.swift
+//  iOS
+//
+//  Created by 史 翔新 on 2020/07/03.
+//
+
+import SwiftUI
+
+struct NameSetupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NameSetupView_Previews: PreviewProvider {
+    static var previews: some View {
+        NameSetupView()
+    }
+}
