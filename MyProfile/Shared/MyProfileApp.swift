30e3f115c065667a47ab425fe156708055412362
史 翔新 <x_shi@yumemi.co.jp>
2020-07-02 21:54:16 +0900

    Initial Xcode project

---
+++ MyProfile/Shared/MyProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProfileApp.swift
+//  Shared
+//
+//  Created by 史 翔新 on 2020/07/02.
+//
+
+import SwiftUI
+
+@main
+struct MyProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
