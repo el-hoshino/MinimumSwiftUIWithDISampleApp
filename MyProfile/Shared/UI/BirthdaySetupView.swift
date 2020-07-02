a592ea1db6e178a7dba00fc21c91c57bb4085243
史 翔新 <x_shi@yumemi.co.jp>
2020-07-03 02:54:55 +0900

    Add BirthdaySetupView file

---
+++ MyProfile/Shared/UI/BirthdaySetupView.swift
@@ -0,0 +1,20 @@
+//
+//  BirthdaySetupView.swift
+//  iOS
+//
+//  Created by 史 翔新 on 2020/07/03.
+//
+
+import SwiftUI
+
+struct BirthdaySetupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BirthdaySetupView_Previews: PreviewProvider {
+    static var previews: some View {
+        BirthdaySetupView()
+    }
+}
